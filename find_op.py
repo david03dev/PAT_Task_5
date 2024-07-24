68ec647a5d10baac916a998a8b3f94845655435d
David Selvaraj <166501046+david03dev@users.noreply.github.com>
2024-07-24 16:25:50 +0530

    Create find_op.py

---
+++ find_op.py
@@ -0,0 +1,11 @@
+#What is the expected output of the following Python code given below :-
+
+data = [10,501,22,37,100,999,87,351]
+result = filter (lambda x : x > 4, data)
+print(list(result))
+
+"""Output:
+
+[10, 501, 22, 37, 100, 999, 87, 351]
+
+"""
